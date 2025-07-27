c541fc73f16a9a9faf9ebbb6eb954f32b5192891
W.S. Meng <meng418312436@gmail.com>
2025-07-27 23:31:58 +0800

    在路由配置中添加 localePrefix 属性，以便在 URL 中根据需要显示默认语言。

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,5 +5,8 @@ export const routing = defineRouting({
   locales: ['en', 'de'],
  
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  
+  // 配置路径命名策略，使默认语言不显示在URL中
+  localePrefix: 'as-needed'
 });
